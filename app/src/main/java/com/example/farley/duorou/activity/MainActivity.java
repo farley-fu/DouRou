da707c11cef00043d054186ccadeaf7ac8a204f5
farley <fuyangily@126.com>
2016-11-30 15:40:51 +0800

    second commit.

--- app/src/main/java/com/example/farley/duorou/activity/MainActivity.java
+++ app/src/main/java/com/example/farley/duorou/activity/MainActivity.java
@@ -11,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
     }
 }
