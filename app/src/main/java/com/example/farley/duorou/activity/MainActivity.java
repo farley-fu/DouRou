f539a62f6d755fe50b4fb2940b2a5fc1f302e34d
farley <fuyangily@126.com>
2016-11-30 15:33:46 +0800

    frist commit.

---
+++ app/src/main/java/com/example/farley/duorou/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.farley.duorou.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.farley.duorou.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/farley/duorou/views/IndexViewPager.java
@@ -0,0 +1,57 @@
+package com.example.farley.duorou.views;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by coollang on 16/11/1.
+ */
+
+public class IndexViewPager extends ViewPager {
+    private boolean noScroll = true;
+
+    public IndexViewPager(Context context) {
+        super(context);
+    }
+
+    public IndexViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public void setNoScroll(boolean noScroll) {
+        this.noScroll = noScroll;
+    }
+
+    @Override
+    public void scrollTo(int x, int y) {
+        super.scrollTo(x, y);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        if (noScroll)
+            return false;
+        else
+            return super.onTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if (noScroll)
+            return false;
+        else
+            return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public void setCurrentItem(int item) {
+        super.setCurrentItem(item);
+    }
+
+    @Override
+    public void setCurrentItem(int item, boolean smoothScroll) {
+        super.setCurrentItem(item, smoothScroll);
+    }
+}
